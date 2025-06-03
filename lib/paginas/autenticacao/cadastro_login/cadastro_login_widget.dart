d21e5878b77a012dbbf8b014b68ad90c1faa1b70
Grupo Aicrus <93139175+Aicrus@users.noreply.github.com>
2025-06-03 12:59:06 -0400

    Merge específico: APENAS UI das telas CadastroLogin e EsqueciSenha do FlutterFlow

--- lib/paginas/autenticacao/cadastro_login/cadastro_login_widget.dart
+++ lib/paginas/autenticacao/cadastro_login/cadastro_login_widget.dart
@@ -5,13 +5,10 @@ import '/componentes/termos_condicoes_email/termos_condicoes_email_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:ui';
 import '/index.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'cadastro_login_model.dart';
 export 'cadastro_login_model.dart';
 
